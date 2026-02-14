b94848743cdf18c00ce15d94a561190389cf93a2
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2026-02-13 21:32:29 -0600

    fix?

--- overlays/codex.nix
+++ overlays/codex.nix
@@ -8,11 +8,5 @@ final: prev: {
       tag = "rust-v${oldAttrs.version}";  # or rev, depending on how upstream tags
       hash = "sha256-m2Jq7fbSXQ/O3bNBr6zbnQERhk2FZXb+AlGZsHn8GuQ=";  # replace with real hash
     };
-
-    cargoDeps = oldAttrs.cargoDeps.overrideAttrs (prev.lib.const {
-      name = "codex-${oldAttrs.version}-vendor.tar.gz";
-      src = oldAttrs.src;
-      outputHash = prev.lib.fakeHash;
-    });
   });
 }
