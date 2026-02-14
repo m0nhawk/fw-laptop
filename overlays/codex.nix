e57498c89439caa42d0cccadc49f15ae3c38fe04
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2026-02-13 21:02:41 -0600

    fix?

--- overlays/codex.nix
+++ overlays/codex.nix
@@ -7,5 +7,6 @@ final: prev: {
       tag = "rust-v${oldAttrs.version}";  # or rev, depending on how upstream tags
       hash = "sha256-m2Jq7fbSXQ/O3bNBr6zbnQERhk2FZXb+AlGZsHn8GuQ=";  # replace with real hash
     };
+    cargoHash = "";
   });
 }
