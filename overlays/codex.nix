e33ac19e16a16ef2b47f1379de13d8ddcfcb2efd
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2026-02-13 21:27:46 -0600

    fix?

--- overlays/codex.nix
+++ overlays/codex.nix
@@ -9,6 +9,10 @@ final: prev: {
       hash = "sha256-m2Jq7fbSXQ/O3bNBr6zbnQERhk2FZXb+AlGZsHn8GuQ=";  # replace with real hash
     };
 
-    cargoHash = "";
+    cargoDeps = oldAttrs.cargoDeps.overrideAttrs (prev.lib.const {
+      name = "codex-${oldAttrs.version}-vendor.tar.gz";
+      src = oldAttrs.src;
+      outputHash = "";
+    });
   });
 }
