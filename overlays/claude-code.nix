96fef105c0687d344fb41f964efed986f508fda9
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2026-02-21 14:54:54 -0600

    Update claude-code.nix to override attributes

--- overlays/claude-code.nix
+++ overlays/claude-code.nix
@@ -1,18 +1,20 @@
-(claude-code.overrideAttrs (
-  final: prev: {
+final: prev: {
+  claude-code = prev.claude-code.overrideAttrs (oldAttrs: let
     version = "2.1.50";
 
-    src = prev.src.override {
+    src = prev.fetchzip {
       url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
       hash = "sha256-pSPZzbLhFsE8zwlp+CHB5MqS1gT3CeIlkoAtswmxCZs=";
     };
+  in {
+    inherit version src;
 
     npmDepsHash = "";
 
-    npmDeps = fetchNpmDeps {
+    npmDeps = final.fetchNpmDeps {
       inherit (final) src;
       name = "${final.pname}-${final.version}-npm-deps";
-      hash = final.npmDepsHash;
+      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
     };
-  }
-))
+  });
+}
